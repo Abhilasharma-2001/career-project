3911439367b9b1e6c8562239129e785b0c7cf38d
Abhilash Sharma <sharmabhilasharma2k@gmail.com>
2026-03-23 09:56:23 +0530

    added logging with timestamp

--- system_monitor.py
+++ system_monitor.py
@@ -1,9 +1,16 @@
 import os
-print("CPU Usage: ")
-os.system("top -b -n1 | head -5")
+from datetime import datetime
 
-print("\n Memory Usage: ")
-os.system("free -h")
+log_file = "system_log.txt"
 
-print("Disk Usage: ")
-os.system("df -h")
+with open(log_file, "a") as f:
+    f.write(f"\n===== {datetime.now()} =====\n")
+
+    f.write("\nCPU Usage:\n")
+    f.write(os.popen("top -b -n1 | head -5").read())
+
+    f.write("\nMemory Usage:\n")
+    f.write(os.popen("free -h").read())
+
+    f.write("\nDisk Usage:\n")
+    f.write(os.popen("df -h").read())
